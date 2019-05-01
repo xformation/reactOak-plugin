14889468f522d164720922096b05ed659c508314
Rajesh <rajeshupadhyay@girnarsoft.com>
2019-05-01 23:09:01 +0530

    Add download implementation

--- app/components/Uploads.js
+++ app/components/Uploads.js
@@ -2,23 +2,43 @@ import { graphql } from 'react-apollo';
 import uploadsQuery from '../queries/uploads';
 import { Table, Head, Cell } from './Table';
 
-const Uploads = ({ data: { uploads = [] } }) => (
-    <Table
-        thead={
-            <tr>
-                <Head>Filename</Head>
-                <Head>MIME type</Head>
-                <Head>Path</Head>
-            </tr>
-        }
-        tbody={uploads.map(({ id, filename, mimetype, path }) => (
-            <tr key={id}>
-                <Cell>{filename}</Cell>
-                <Cell>{mimetype}</Cell>
-                <Cell>{path}</Cell>
-            </tr>
-        ))}
-    />
-);
+import fs from 'fs';
+
+const Uploads = ({ data: { uploads = [] } }) => {
+    uploads.map(({ id, path, nodePath }) => {
+        fs.access(path, fs.F_OK, (err) => {
+            if (err) {
+                console.error(err);
+                const fPath = nodePath + "/" + path.substring(path.lastIndexOf("/") + 1);
+                axios.post(process.env.OAK_SERVER + "/download", {
+                    path: fPath
+                }).then((response) => {
+                    console.log("Headers: ", response.headers);
+                }).catch(function (error) {
+                    console.log("Create Err: ", error);
+                });
+            }
+            //file exists
+        });
+    });
+    return (
+        <Table
+            thead={
+                <tr>
+                    <Head>Filename</Head>
+                    <Head>MIME type</Head>
+                    <Head>Path</Head>
+                </tr>
+            }
+            tbody={uploads.map(({ id, filename, mimetype, path }) => (
+                <tr key={id}>
+                    <Cell>{filename}</Cell>
+                    <Cell>{mimetype}</Cell>
+                    <Cell>{path}</Cell>
+                </tr>
+            ))}
+        />
+    );
+}
 
 export default graphql(uploadsQuery)(Uploads);
--- app/queries/uploads.js
+++ app/queries/uploads.js
@@ -7,6 +7,7 @@ export default gql`
       filename
       mimetype
       path
+      nodePath
     }
   }
 `;
